8997d521a7ce4bb7d8d89de9125caefba601497e
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2026-02-05 18:41:20 +0500

    config: add TypeORM configuration to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'wallet',
+      password: 'wallet123',
+      database: 'wallet',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: false,
+    }),
+    WalletModule,
+  ],
 })
 export class AppModule {}
