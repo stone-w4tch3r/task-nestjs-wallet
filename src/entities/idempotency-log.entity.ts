2339f4568f782c4d03c8a47b3352fd1375fd4cad
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2026-02-05 18:40:40 +0500

    feat: add IdempotencyLog entity

---
+++ src/entities/idempotency-log.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity('idempotency_logs')
+export class IdempotencyLog {
+  @PrimaryColumn()
+  key: string;
+
+  @Column({ type: 'json' })
+  response: Record<string, unknown>;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
